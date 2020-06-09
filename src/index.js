43c62511cc8fa1892d4a6f3edb13998c7a7f7de6
Felipe Leão <felipe@felipeleao.com>
2020-06-09 13:45:16 -0300

    Allow users to join

--- src/index.js
+++ src/index.js
@@ -1,41 +1,43 @@
-require('dotenv').config()
-const path = require('path')
-const express = require('express')
-const http = require('http')
-const socketio = require('socket.io')
-const { generateMessage } = require('./utils/messages')
-const { HOST, PORT } = process.env
+require("dotenv").config();
+const path = require("path");
+const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
+const { generateMessage } = require("./utils/messages");
+const { HOST, PORT } = process.env;
 
-const application = express()
-const server = http.createServer(application)
-const io = socketio(server)
+const application = express();
+const server = http.createServer(application);
+const io = socketio(server);
 
-const publicDirectoryPath = path.join(__dirname, 'public')
+const publicDirectoryPath = path.join(__dirname, "public");
 
-application.use(express.static(publicDirectoryPath))
+application.use(express.static(publicDirectoryPath));
 
-io.on('connection', socket => {
-  console.log('New Socket Connection')
+io.on("connection", (socket) => {
+  console.log("New Socket Connection");
 
-  // Send message for only person who connects
-  socket.emit('message', generateMessage('Welcome'))
+  socket.on("join", ({ username, room }) => {
+    socket.join(room);
+    socket.emit("message", generateMessage("Welcome"));
+    socket.broadcast
+      .to(room)
+      .emit("message", generateMessage(`${username} has joined!`));
+  });
 
-  // Sends to everyone but the connected person who connects
-  socket.broadcast.emit('message', generateMessage('A new user has joined!'))
-
-  socket.on('sendMessage', (message, delivered) => {
+  socket.on("sendMessage", (message, delivered) => {
     // Emit for everyone connected
-    io.emit('message', generateMessage(message))
+    io.to("bh").emit("message", generateMessage(message));
 
     // Confirm to sender if message was delivered
-    delivered('Delivered.')
-  })
+    delivered("Delivered.");
+  });
 
-  socket.on('disconnect', () => {
-    io.emit('message', generateMessage('A user has left!'))
-  })
-})
+  socket.on("disconnect", () => {
+    io.to("bh").emit("message", generateMessage("A user has left!"));
+  });
+});
 
 server.listen(PORT, () => {
-  console.log(`Server running on ${HOST}:${PORT}`)
-})
+  console.log(`Server running on ${HOST}:${PORT}`);
+});
--- src/public/chat.js
+++ src/public/chat.js
@@ -1,31 +1,38 @@
-const socket = window.io()
-const $messageForm = document.querySelector('#message-form')
-const $messageFormInput = $messageForm.querySelector('input')
-const $messageFormButton = $messageForm.querySelector('button')
-const $messages = document.querySelector('#messages')
+const socket = window.io();
+const $messageForm = document.querySelector("#message-form");
+const $messageFormInput = $messageForm.querySelector("input");
+const $messageFormButton = $messageForm.querySelector("button");
+const $messages = document.querySelector("#messages");
 
-const messageTemplate = document.querySelector('#message-template').innerHTML
+const messageTemplate = document.querySelector("#message-template").innerHTML;
 
-socket.on('message', message => {
+// OPTIONS
+const { username, room } = window.Qs.parse(window.location.search, {
+  ignoreQueryPrefix: true,
+});
+
+socket.on("message", (message) => {
   const html = window.Mustache.render(messageTemplate, {
     message: message.text,
-    createdAt: window.moment(message.createdAt).format('h:mm a')
-  })
-  $messages.insertAdjacentHTML('beforeend', html)
-})
+    createdAt: window.moment(message.createdAt).format("h:mm a"),
+  });
+  $messages.insertAdjacentHTML("beforeend", html);
+});
 
-$messageForm.addEventListener('submit', e => {
-  e.preventDefault()
+$messageForm.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-  $messageFormButton.setAttribute('disabled', 'disabled')
+  $messageFormButton.setAttribute("disabled", "disabled");
 
-  const message = e.target.elements.message.value
-  socket.emit('sendMessage', message, message => {
-    $messageFormButton.removeAttribute('disabled')
-    $messageFormInput.value = ''
-    $messageFormInput.focus()
+  const message = e.target.elements.message.value;
+  socket.emit("sendMessage", message, (message) => {
+    $messageFormButton.removeAttribute("disabled");
+    $messageFormInput.value = "";
+    $messageFormInput.focus();
 
     // confirms server reads the message
-    console.log('Status: ', message)
-  })
-})
+    console.log("Status: ", message);
+  });
+});
+
+socket.emit("join", { username, room });
