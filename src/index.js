2052bd716b649d2dedd70371879760119a4b6017
Felipe Leão <felipe@felipeleao.com>
2020-06-09 14:36:03 -0300

    Add sidebar with users

--- src/index.js
+++ src/index.js
@@ -39,6 +39,11 @@ io.on("connection", (socket) => {
         generateMessage("Admin", `${user.username} has joined!`)
       );
 
+    io.to(user.room).emit("roomData", {
+      room: user.room,
+      users: getUsersInRoom(user.room),
+    });
+
     res();
   });
 
@@ -59,6 +64,11 @@ io.on("connection", (socket) => {
         "message",
         generateMessage("Admin", `${user.username} has left!`)
       );
+
+      io.to(user.room).emit("roomData", {
+        room: user.room,
+        users: getUsersInRoom(user.room),
+      });
     }
   });
 });
--- src/public/chat.js
+++ src/public/chat.js
@@ -5,6 +5,7 @@ const $messageFormButton = $messageForm.querySelector("button");
 const $messages = document.querySelector("#messages");
 
 const messageTemplate = document.querySelector("#message-template").innerHTML;
+const sidebarTemplate = document.querySelector("#sidebar-template").innerHTML;
 
 // OPTIONS
 const { username, room } = window.Qs.parse(window.location.search, {
@@ -18,6 +19,15 @@ socket.on("message", (message) => {
     createdAt: window.moment(message.createdAt).format("h:mm a"),
   });
   $messages.insertAdjacentHTML("beforeend", html);
+});
+
+socket.on("roomData", ({ room, users }) => {
+  const html = window.Mustache.render(sidebarTemplate, {
+    room,
+    users,
+  });
+
+  document.querySelector("#sidebar").innerHTML = html;
 });
 
 $messageForm.addEventListener("submit", (e) => {
